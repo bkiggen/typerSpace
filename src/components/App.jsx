4a646a024dea9381d29f5e1cb55fac3930632a0d
Student <benkiggen@gmail.com>
2018-12-07 10:32:20 -0800

    development server up and running

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  super(props);
-  constructor(){
 
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   render() {
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+import thunkMiddleware from 'redux-thunk';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
