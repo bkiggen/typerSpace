334616e9b1152cae1fcb69b6f102d3561ef90a82
Student <benkiggen@gmail.com>
2018-12-19 13:51:38 -0800

    add text array to battle

--- src/components/Battle.jsx
+++ src/components/Battle.jsx
@@ -22,7 +22,6 @@ const Canvas = styled.div`
 `;
 
 const Half = styled.div`
-
   ${props => props.left && css`
     background-color: blue;
     width: 50%;
@@ -33,50 +32,75 @@ const Half = styled.div`
     background-color: green;
     width: 50%;
     height: 100%;
+    padding: 10px;
+    font-size: 22px;
   `}
-
 `
-
-
-function Battle(props){
-
-  document.onkeypress = function(e){
-    e = e || window.event;
-    var charCode = e.keyCode || e.which;
-    var charStr = String.fromCharCode(charCode);
-    console.log(charStr);
-
-  }
-
-  function checkLetterInput(keyPressed){
-    let targetLetter = typingContentArray[props.currentLetterPosition];
-    const { dispatch } = props;
-    if(keyPressed === targetLetter){
-      const action = {
-        type: c.UPDATE_CURRENT_LETTER
-      };
-      const secondAction = {
-        type: c.UPDATE_LETTERS_CORRECT
-      };
-      dispatch(action);
-      dispatch(secondAction);
-      if(props.currentLetterPosition === typingContentArray.length - 1){
-        endRound();
-      }
-    } else if (keyPressed !== targetLetter){
-      const incorrectDispatch = {
-        type: c.UPDATE_LETTERS_INCORRECT
-      }
-      dispatch(incorrectDispatch);
-    }
-  };
-
 
 const Lander = styled.img`
   position: absolute;
   top: 5px;
   left: 221px;
+  animation: wiggle 2s infinite linear;
+  @keyframes wiggle {
+    25%{
+      transform: rotateZ(5deg)
+    }
+    75% {
+      transform: rotateZ(-5deg)
+    }
+  }
 `
+
+const Span = styled.span`
+  color: red;
+  text-decoration: underline;
+`
+
+function Battle(props){
+
+  let typingContentString = `No one would have believed in the last years of the nineteenth century that this world was being watched keenly and closely by intelligences greater than man’s and yet as mortal as his own; that as men busied themselves about their various concerns they were scrutinised and studied, perhaps almost as narrowly as a man with a microscope might scrutinise the transient creatures that swarm and multiply in a drop of water. With infinite complacency men went to and fro over this globe about their little affairs, serene in their assurance of their empire over matter. It is possible that the infusoria under the microscope do the same. No one gave a thought to the older worlds of space as sources of human danger, or thought of them only to dismiss the idea of life upon them as impossible or improbable. It is curious to recall some of the mental habits of those departed days. At most terrestrial men fancied there might be other men upon Mars, perhaps inferior to themselves and ready to welcome a missionary enterprise. Yet across the gulf of space, minds that are to our minds as ours are to those of the beasts that perish, intellects vast and cool and unsympathetic, regarded this earth with envious eyes, and slowly and surely drew their plans against us. And early in the twentieth century came the great disillusionment.`;
+  let typingContentArray = typingContentString.split('');
+
+  document.onkeypress = function(e){
+    e = e || window.event;
+    var charCode = e.keyCode || e.which;
+    var charStr = String.fromCharCode(charCode);
+    checkLetterInput();
+    console.log(charStr);
+  }
+
+  let currentLetterPosition = 0;
+
+  function getTypingContent(keyPressed){
+    let displayContent = [];
+    for(let i = 0; i < typingContentArray.length; i++){
+      if(i === currentLetterPosition) {
+        displayContent.push(<Span>{typingContentArray[i]}</Span>);
+      } else {
+        displayContent.push(<span>{typingContentArray[i]}</span>);
+      }
+    }
+    let targetLetter = typingContentArray[currentLetterPosition];
+    return displayContent;
+  }
+
+  function checkLetterInput(keyPressed){
+    console.log(typingContentArray);
+    let targetLetter = typingContentArray[currentLetterPosition];
+    const { dispatch } = props;
+    if(keyPressed === targetLetter){
+      currentLetterPosition += 1;
+      if(currentLetterPosition === typingContentArray.length - 1){
+        //do something cool
+      }
+    } else if (keyPressed !== targetLetter){
+
+    }
+  };
+
+
+
 
   return (
     <BattleDiv>
@@ -84,7 +108,9 @@ const Lander = styled.img`
         <Half left>
           <Lander src={usaShip}/>
         </Half>
-        <Half right></Half>
+        <Half right>
+          {getTypingContent()}
+        </Half>
       </Canvas>
     </BattleDiv>
   );
